90692555a655d83b45b8e57d8cd49c817006cd37
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:19:24 +0300

    refactor: move freeipapi.js to src directory

---
+++ src/freeipapi.js
@@ -0,0 +1,11 @@
+class freeipapi{
+    constructor(){
+        this.api = "https://freeipapi.com/api"
+        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0","x-requested-with": "xmlhttprequest","content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
+    }
+    async my_ip(){
+        let req=await fetch(`${this.api}/json/`,{method:"GET",headers: this.headers});
+        return await req.json();
+    }
+}
+module.exports = {freeipapi};
